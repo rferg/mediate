54b9675cf49d360480aa2b419c6196e4759c7675
rferg <rmferguson77@gmail.com>
2022-07-18 16:40:56 -0400

    tests for RequestHandler

---
+++ spec/mediate/request_handler_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "../stubs/basic"
+
+RSpec.describe Mediate::RequestHandler do
+  describe "#handles" do
+    it "calls register_request_handler on Mediator" do
+      mediator = instance_double("Mediate::Mediator")
+      request_class = Stubs::Request
+      expect(mediator).to receive(:register_request_handler).with(Mediate::RequestHandler, request_class)
+      Mediate::RequestHandler.handles(request_class, mediator)
+    end
+  end
+
+  describe "#handle" do
+    it "should raise NoMethodError" do
+      expect { Mediate::RequestHandler.handle(nil) }.to raise_error(NoMethodError)
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,8 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+  
+  config.mock_with :rspec do |c|
+    c.verify_doubled_constant_names = true
+  end
 end
